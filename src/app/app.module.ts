341340f8e1c9d2aabf8f4a9d8f39688f54c2dbf2
AlanYoel <alanyoelsancheza@gmail.com>
2023-06-27 18:25:32 -0400

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Rutas
+import { APP_ROUTES } from './app.routes';
+
+
 import { AppComponent } from './app.component';
+
+import { NopagesfoundComponent } from './shared/nopagesfound/nopagesfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/login/register.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressComponent,
+    NopagesfoundComponent,
+    DashboardComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    LoginComponent,
+    RegisterComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
